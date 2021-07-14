247c47a4353ecc3b3b93469df28e84369a9c5556
Skanda2007 <82853099+Skanda2007@users.noreply.github.com>
2021-07-14 13:02:37 +0530

    Added with while loop
    Added while loop ➿

--- Main.c
+++ Main.c
@@ -17,14 +17,41 @@ int main()
         getchar();
         i++;
     }
-    printf("i == %d\n", i);
+    //printf("i == %d\n", i);
     //i==get
-    //correct down here👇
     printf("Numbers entered are:- ");
     for(int i = get- 1;i>=0;i--)
     {
         //printf("entered loop");
         //break;
         printf("%d,", a[i]);
+    }
+}
+//With While Loop
+#include <stdio.h>
+int main()
+{
+    int a[100];
+    int get;
+    int i=0;
+    
+    printf("Enter the number of number you want to enter:- ");
+    scanf("%d", &get);
+    //printf("Number entered is %d\n", get);
+    while(i<get)
+    {
+        printf("enter a number:- ");
+        scanf("%d", &a[i]);
+       // printf("You have entered %d\n",a[i]);
+        getchar();
+        i++;
+    }
+    printf("i == %d\n", i);
+    i=get- 1;
+    printf("Numbers entered are:- ");
+    while(i>=0)
+    {
+        printf("%d,", a[i]);
+        i--;
     }
 }
